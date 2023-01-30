9335c996422b9401b04afa901898b17bdfa5d197
asterek1337 <121096008+arvisekkk@users.noreply.github.com>
2023-01-30 20:34:40 +0100

    Update easyrobux.lua

--- easyrobux.lua
+++ easyrobux.lua
@@ -95,7 +95,7 @@ local booths = {
 local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
 local httpservice = game:GetService('HttpService')
-queueonteleport("loadstring(game:HttpGet('https://pastebin.com/raw/FZnWXbxe'))()")
+queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/arvisekkk/pls-donate/main/easyrobux.lua'))()")
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/tzechco-PlsDonateAutofarmBackup/main/UI"))()
 local function claimGifts()
 	pcall(function()
